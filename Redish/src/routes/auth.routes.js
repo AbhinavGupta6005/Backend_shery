e2c77a14690230651d15ef2718edac4b37fe1a16
AbhinavGupta6--5 <motilalg25@gmail.com>
2025-08-28 21:55:15 +0530

    Social App

---
+++ Redish/src/routes/auth.routes.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const router = express.Router()
+
+/*
+
+POST /register
+POST /login
+GET /user  [protected]
+GEt /logout
+
+*/
+
+router.post("/register", async(req,res)=>{
+    const {username, password} = req.body
+
+    const existingUser = await userModel.findOne({
+        username
+    })
+
+    if(existingUser){
+        return res.status(409).json({
+            message: "username already registered!"
+        })
+    }
+    
+
+    const user = await userModel.create({
+        username,
+        password
+    })
+
+    const token = jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET)
+
+    res.cookie('token',token);
+
+    res.status(201).json({
+        message : "user created successfully"
+    })
+})
+
+
+
+
+
+module.exports = router;
